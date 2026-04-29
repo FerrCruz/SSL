b8c5d95735ded4b41a516e016daacd0ed3fe3291
FerrCruz <cfer3890@gmail.com>
2026-04-28 23:35:14 -0300

    front page del readme actualizado

--- 00-CHelloWorld/hello.c
+++ 00-CHelloWorld/hello.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello world");
+    printf("hello wortld\n");
+
+   #ifdef __STDC_VERSION__
+
+    long version = __STDC_VERSION__;
+    if (version == 202311L) printf("C23 standard\n");
+    else if (version == 201710L) printf("C17 standard\n");
+    else if (version == 201112L) printf("C11 standard\n");
+    else if (version == 199901L) printf("C99 standard\n");
+    else printf("Standard version macro: %ld\n", version);
+#else
+    printf("Pre-C99 or non-standard compliant compiler\n");
+#endif
     return 0;
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-front page del repositorio
+/*front page del repositorio*/
+
+Compilador Seleccionado: GCC
+Version del Compilador: 15.2.0
+Version C del Compilador: Estandar C23
